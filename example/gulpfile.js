06c9f3b2994e76001cfd8efba71fc231cc131935
Valentin Sirbu <valentinsirbu@yandex.com>
2016-12-14 12:52:46 +0200

    example upd

---
+++ example/gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+//Import project config
+var config = require('./config');
+var gulpHelper = require('./gulp-helper');
+//Environment
+var isDev = ! gutil.env.production;
+//Init Gulp Helper
+gulpHelper.init(gulp, config, isDev);
