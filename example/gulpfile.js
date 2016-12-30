918a2d5d0572f402be037d94b204ece4bce53461
Valentin Sirbu <valentinsirbu@yandex.com>
2016-12-30 18:51:13 +0200

    fix task not found error

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 //Import project config
 var config = require('./config');
-var gulpHelper = require('./gulp-helper');
+var gulpHelper = require('gulp-build-helper');
 //Environment
 var isDev = ! gutil.env.production;
 //Init Gulp Helper
