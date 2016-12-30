48a3fb889a20f6954bc45fbea602883aafd089fd
Valentin Sirbu <valentinsirbu@yandex.com>
2016-12-30 19:00:27 +0200

    update example

---
+++ example/assets/js/app.js
@@ -0,0 +1,12 @@
+var app =
+webpackJsonp_name_([0,1],[
+/* 0 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	console.log('Hellow World');
+
+/***/ }
+]);
+//# sourceMappingURL=maps/app.js.map
